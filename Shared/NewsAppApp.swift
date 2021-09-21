c2c683d8e01a424e0dc3dd51e14d0ad0e1dadeb4
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-09-21 15:52:02 +0100

    Initial Commit

---
+++ Shared/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  Shared
+//
+//  Created by Matthew Folbigg on 21/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
