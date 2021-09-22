057203fb49cb6b23c8be99f32fd1e8e831c6b1bd
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-09-22 19:57:31 +0100

    Separated GuardianArticle from response

---
+++ Shared/GuardianArticle.swift
@@ -0,0 +1,79 @@
+//
+//  GuardianArticle.swift
+//  NewsApp
+//
+//  Created by Matthew Folbigg on 22/09/2021.
+//
+
+import Foundation
+
+struct GuardianArticle: Codable, Identifiable {
+    let id: String
+    let type: String
+    let sectionID: String
+    let sectionName: String
+    let publicationDate: String
+    let title: String
+    let webURL: String
+    let apiURL: String
+    let isHosted: Bool
+    let pillarID: String
+    let pillarName: String
+    let additional: Additional?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case type
+        case sectionID = "sectionId"
+        case sectionName
+        case publicationDate = "webPublicationDate"
+        case title = "webTitle"
+        case webURL = "webUrl"
+        case apiURL = "apiUrl"
+        case isHosted
+        case pillarID = "pillarId"
+        case pillarName
+        case additional = "fields"
+    }
+    
+    var compactDate: String {
+        let ISOFormatter = ISO8601DateFormatter()
+        let date = ISOFormatter.date(from: self.publicationDate)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMM"
+//        formatter.dateStyle = .short
+        return formatter.string(from: date!)
+    }
+    
+    var attributedBody: String {
+        let htmlBody = Data(self.additional?.bodyHtml?.utf8 ?? "".utf8)
+        let attr = try? NSAttributedString(data: htmlBody, options: [
+                    .documentType: NSAttributedString.DocumentType.html,
+                    .characterEncoding: String.Encoding.utf8.rawValue
+                ], documentAttributes: nil)
+        return attr?.string ?? ""
+    }
+    
+}
+
+struct Additional: Codable {
+    let thumbnailURL: String?
+    let publication: String?
+    let starRating: String?
+    let trailText: String?
+    let wordcount: String?
+    let byline: String?
+    let bodyText: String?
+    let bodyHtml: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case thumbnailURL = "thumbnail"
+        case publication
+        case starRating
+        case trailText
+        case wordcount
+        case byline
+        case bodyText
+        case bodyHtml = "body"
+    }
+}
